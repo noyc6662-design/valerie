9bd6b23b5874312b2c4801461dc908a4b7a31ca8
noyc6662-design <noyc6662@gmail.com>
2026-02-07 10:55:36 +0000

    Fix: Comment out i18n config incompatible with app directory

--- next.config.mjs
+++ next.config.mjs
@@ -15,10 +15,10 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
   images: {
     unoptimized: true,
     domains: ["image.tmdb.org"],
